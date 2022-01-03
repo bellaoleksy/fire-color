d06aa14c0a7968990bc3292cff1df6a5bd79e92d
Bella Oleksy <36306783+bellaoleksy@users.noreply.github.com>
2022-01-03 15:31:42 -0700

    clean up libraries script with pacman

--- scripts/00_libraries.R
+++ scripts/00_libraries.R
@@ -1,62 +1,45 @@
 ##Load all the libraries your heart desires
 
+if (!require('pacman')) install.packages('pacman'); library('pacman')
 
-# install.packages(c("cowplot", "googleway", "ggplot2", "ggrepel", 
-# "ggspatial", "libwgeom", "sf", "rnaturalearth", "rnaturalearthdata"))
-if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
-if (!require('colorspace')) install.packages('colorspace'); library('colorspace')#Borrowed some code from their minimum working example: 
-if (!require('formatR')) install.packages('formatR'); library('formatR')
-if (!require('feather')) install.packages('feather'); library('feather')
-if (!require('zoo')) install.packages('zoo'); library('zoo') #rollapply()
-if (!require('furrr')) install.packages('furrr'); library('furrr') 
-if (!require('xts')) install.packages('xts'); library('xts') 
-if (!require('imputeTS')) install.packages('imputeTS'); library('imputeTS') 
-if (!require('purrr')) install.packages('purrr'); library('purrr') 
-if (!require('changepoint')) install.packages('changepoint'); library('changepoint') 
-
-#PCAs
-if (!require('ggfortify')) install.packages('ggfortify');library(ggfortify)
-if (!require('cluster')) install.packages('cluster');library(cluster)
-if (!require('FactoMineR')) install.packages('FactoMineR');library("FactoMineR")
-if (!require('factoextra')) install.packages('factoextra');library("factoextra")
-
-
-library(tidyverse)
-library(lubridate)
-if (!require('patchwork')) install.packages('patchwork'); library('patchwork')
-if (!require('kableExtra')) install.packages('kableExtra'); library('kableExtra')
-library(data.table) #for faster data summaries than dplyr
-if (!require('ggstatsplot')) install.packages('ggstatsplot'); library('ggstatsplot')
-if (!require('ggthemes')) install.packages('ggthemes'); library('ggthemes')
-theme_set(ggthemes::theme_base())
-if (!require('GGally')) install.packages('GGally'); library('GGally')
-
-
-#For summaries
-if (!require('huxtable')) install.packages('huxtable'); library('huxtable')
-if (!require('officer')) install.packages('officer'); library('officer')
-if (!require('flextable')) install.packages('flextable'); library('flextable')
-
-#Mapping
-if (!require('sf')) install.packages('sf'); library("sf")
-if (!require('rnaturalearth')) install.packages('rnaturalearth'); library("rnaturalearth")
-if (!require('rnaturalearthdata')) install.packages('rnaturalearthdata'); library("rnaturalearthdata")
-if (!require('ggspatial')) install.packages('ggspatial'); library("ggspatial")
-if (!require('raster')) install.packages('raster'); library(raster)
-if (!require('tidytext')) install.packages('tidytext'); library(tidytext)
-if (!require('nhdplusTools')) install.packages('nhdplusTools'); library('nhdplusTools')
-if (!require('maps')) install.packages('maps'); library('maps')
-if (!require('rgeos')) install.packages('rgeos'); library('rgeos')
-
-if (!require('ggfortify')) install.packages('ggfortify'); library('ggfortify')
-if (!require('cluster')) install.packages('cluster'); library('cluster')
-if (!require('FactoMineR')) install.packages('FactoMineR'); library('FactoMineR')
-if (!require('factoextra')) install.packages('factoextra'); library('factoextra')
-
-#For sen slopes
-library(trend)
-library(zyp)
-
-#Wilcox tests
-
-if (!require('coin')) install.packages('coin'); library('coin')
+pacman::p_load("ggplot2",
+               "colorspace",
+               "formatR",
+               "feather",
+               "zoo", #rollapply()
+               "furrr",
+               "xts",
+               "imputeTS",
+               "purrr",
+               "changepoint",
+               "corrplot",
+               "ggfortify",
+               "cluster",
+               "FactoMineR",
+               "factoextra",
+               "tidyverse",
+               "lubridate",
+               "patchwork",
+               "kableExtra",
+               "ggstatsplot",
+               "ggthemes",
+               "ggpubr",
+               "GGally",
+               "data.table",
+               "huxtable",
+               "officer",
+               "flextable",
+               "sf",
+               "rnaturalearth",
+               "rnaturalearthdata",
+               "ggspatial",
+               "raster",
+               "tidytext",
+               "nhdplusTools",
+               "maps",
+               "rgeos",
+               "ggfortify",
+               "trend", #for sen slopes
+               "zyp", #for sen slopes
+               "coin"
+               )
